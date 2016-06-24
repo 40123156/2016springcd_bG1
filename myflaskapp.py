da2b28c7d8194664628c22c7aa3a006b93e0952a
40123134 <40123134@gm.nfu.edu.tw>
2016-06-24 16:50:55 +0800

    修改

--- myflaskapp.py
+++ myflaskapp.py
@@ -9,7 +9,6 @@ import init
 #bg1
 import users.b.g1.bg1_40123156
 import users.b.g1.b40123133_2D
-import users.b.g1.bg1_40123131
 
 # 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -388,4 +387,3 @@ if __name__ == "__main__":
 #bg1
 app.register_blueprint(users.b.g1.bg1_40123156.bg1_40123156)
 app.register_blueprint(users.b.g1.b40123133_2D.scrum40123133_task1)
-app.register_blueprint(users.b.g1.bg1_40123131.bg1)
