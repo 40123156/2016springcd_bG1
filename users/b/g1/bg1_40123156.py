468e62cd5af04353c26f75a44763764f249d2ea3
40123156 <40123156@gm.nfu.edu.tw>
2016-06-27 21:56:28 +0800

    新增 2D 圖形的步驟和心得

--- users/b/g1/bg1_40123156.py
+++ users/b/g1/bg1_40123156.py
@@ -5,9 +5,11 @@ from flask import Blueprint, render_template, make_response
 bg1_40123156 = Blueprint('bg1_40123156', __name__, url_prefix='/bg1_40123156', template_folder='templates')
 
 
-@bg1_40123156.route('/abcd')
+@bg1_40123156.route('/circle')
 def three():
     outstring = '''
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,14 +21,18 @@ def three():
 <script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango-8v03.js"></script>
 <script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango2D-6v13.js"></script>
 <script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/CangoAxes-1v33.js"></script>
+ 
 </head>
 <body>
+ 
 <script>
 window.onload=function(){
 brython(1);
 }
 </script>
+ 
 <canvas id="plotarea" width="800" height="800"></canvas>
+
 <script type="text/python">
 from javascript import JSConstructor
 from browser import alert
@@ -54,33 +60,33 @@ deg = math.pi/180
  
 # 將繪製鏈條輪廓的內容寫成 class 物件
 class chain():
-    # 輪廓的外型設為成員變數
-    chamber = "M -6.8397, -1.4894 \
-            A 7, 7, 0, 1, 0, 6.8397, -1.4894 \
-            A 40, 40, 0, 0, 1, 6.8397, -18.511 \
-            A 7, 7, 0, 1, 0, -6.8397, -18.511 \
-            A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
+    # 輪廓的外型設為 class variable
+    chamber = "M50.400 31.200 L51.000,5.000 L5.000,5.200 L5.200,73.200 L48.600,72.800 L48.800,44.000 L37.600,44.400 L37.400,62.200 L15.000,62.200 L15.800,15.600 L37.200,15.400 L37.500,31.800 L50.400,31.200 "
+    #chamber = "M 0, 0 L 0, -20 z"
     cgoChamber = window.svgToCgoSVG(chamber)
  
-    # 利用鏈條起點與終點定義繪圖, 使用內定的 color, border 與 linewidth 變數
-    def basic(self, x1, y1, x2, y2, color="green", border=True, linewidth=4, scale=1):
+    def __init__(self, fillcolor="green", border=True, strokecolor= "tan", linewidth=2, scale=1):
+        self.fillcolor = fillcolor
+        self.border = border
+        self.strokecolor = strokecolor
+        self.linewidth = linewidth
+        self.scale = scale
+ 
+    # 利用鏈條起點與終點定義繪圖
+    def basic(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-        self.color = color
-        self.border = border
-        self.linewidth = linewidth
-        self.scale = scale
         # 注意, cgo.Chamber 為成員變數
         cmbr = cobj(self.cgoChamber, "SHAPE", {
-                "fillColor": color,
-                "border": border,
-                "strokeColor": "tan",
-                "lineWidth": linewidth })
+                "fillColor": self.fillcolor,
+                "border": self.border,
+                "strokeColor": self.strokecolor,
+                "lineWidth": self.linewidth })
  
         # hole 為原點位置
-        hole = cobj(shapedefs.circle(4), "PATH")
+        hole = cobj(shapedefs.circle(4*self.scale), "PATH")
         cmbr.appendPath(hole)
  
         # 複製 cmbr, 然後命名為 basic1
@@ -89,30 +95,28 @@ class chain():
         basic1.rotate(math.atan2(y2-y1, x2-x1)/deg+90)
  
         # 放大 scale 倍
-        cgo.render(basic1, x1, y1, scale, 0)
+        cgo.render(basic1, x1, y1, self.scale, 0)
  
     # 利用鏈條起點與旋轉角度定義繪圖, 使用內定的 color, border 與 linewidth 變數
-    def basic_rot(self, x1, y1, rot, color="green", border=True, linewidth=4, scale=1):
+    def basic_rot(self, x1, y1, rot, v=False):
+        # 若 v 為 True 則為虛擬 chain, 不 render
         self.x1 = x1
         self.y1 = y1
         self.rot = rot
-        self.color = color
-        self.border = border
-        self.linewidth = linewidth
-        self.scale = scale
-        # 注意, cgo.Chamber 為成員變數
+        self.v = v
+        # 注意, cgoChamber 為成員變數
         cmbr = cobj(self.cgoChamber, "SHAPE", {
-                "fillColor": color,
-                "border": border,
-                "strokeColor": "tan",
-                "lineWidth": linewidth })
+                "fillColor": self.fillcolor,
+                "border": self.border,
+                "strokeColor": self.strokecolor,
+                "lineWidth": self.linewidth })
  
-        # hole 為原點位置
-        hole = cobj(shapedefs.circle(4), "PATH")
+        # hole0 為原點位置
+        hole = cobj(shapedefs.circle(4*self.scale), "PATH")
         cmbr.appendPath(hole)
         # 根據旋轉角度, 計算 x2 與 y2
-        x2 = x1 + 20*math.cos(rot*deg)
-        y2 = y1 + 20*math.sin(rot*deg)
+        x2 = x1 + 20*math.cos(rot*deg)*self.scale
+        y2 = y1 + 20*math.sin(rot*deg)*self.scale
  
         # 複製 cmbr, 然後命名為 basic1
         basic1 = cmbr.dup()
@@ -120,99 +124,27 @@ class chain():
         basic1.rotate(rot+90)
  
         # 放大 scale 倍
-        cgo.render(basic1, x1, y1, scale, 0)
+        if v == False:
+            cgo.render(basic1, x1, y1, self.scale, 0)
  
         return x2, y2
- 
-# 利用 chain class 建立案例, 對應到 mychain 變數
+
 mychain = chain()
  
-# 畫 B
-# 左邊四個垂直單元
-x1, y1 = mychain.basic_rot(0, 0, 90)
-x2, y2 = mychain.basic_rot(x1, y1, 90)
-x3, y3 = mychain.basic_rot(x2, y2, 90)
-x4, y4 = mychain.basic_rot(x3, y3, 90)
-# 上方一個水平單元
-x5, y5 = mychain.basic_rot(x4, y4, 0)
-# 右斜 -30 度
-x6, y6 = mychain.basic_rot(x5, y5, -30)
-# 右上垂直向下單元
-x7, y7 = mychain.basic_rot(x6, y6, -90)
-# 右斜 240 度
-x8, y8 = mychain.basic_rot(x7, y7, 210)
-# 中間水平
-mychain.basic(x8, y8, x2, y2)
-# 右下斜 -30 度
-x10, y10 = mychain.basic_rot(x8, y8, -30)
-# 右下垂直向下單元
-x11, y11 = mychain.basic_rot(x10, y10, -90)
-# 右下斜 240 度
-x12, y12 = mychain.basic_rot(x11, y11, 210)
-# 水平接回起點
-mychain.basic(x12,y12, 0, 0, color="green")
+x1, y1 = mychain.basic_rot(0,0, 50)
+x2, y2=mychain.basic_rot(x1, y1, 90-80) 
+x3, y3=mychain.basic_rot(x2, y2, 90-120) 
+x4, y4=mychain.basic_rot(x3, y3, 90-160) 
+x5, y5=mychain.basic_rot(x4, y4, 90-200) 
+x6, y6=mychain.basic_rot(x5, y5, 90-240) 
+x7, y7=mychain.basic_rot(x6, y6, 90-280) 
+x8, y8=mychain.basic_rot(x7, y7, 90-320) 
+x9, y9=mychain.basic_rot(x8, y8, 90-360) 
 
-
-cgo.setWorldCoords(-247.5, -350, 500, 500) 
-# 畫 A
-# 左邊兩個垂直單元
-x1, y1 = mychain.basic_rot(0, 0, 90)
-x2, y2 = mychain.basic_rot(x1, y1, 90)
-# 左斜邊兩個單元
-x3, y3 = mychain.basic_rot(x2, y2, 80)
-x4, y4 = mychain.basic_rot(x3, y3, 71)
-# 最上方水平單元
-x5, y5 = mychain.basic_rot(x4, y4, 0)
-# 右斜邊兩個單元
-x6, y6 = mychain.basic_rot(x5, y5, -71)
-x7, y7 = mychain.basic_rot(x6, y6, -80)
-# 右邊兩個垂直單元
-x8, y8 = mychain.basic_rot(x7, y7, -90)
-x9, y9 = mychain.basic_rot(x8, y8, -90)
-# 中間兩個水平單元
-x10, y10 = mychain.basic_rot(x8, y8, -180)
-mychain.basic(x10, y10, x1, y1, color="green")
-
-cgo.setWorldCoords(-55, -50, 500, 500) 
-# 畫 D
-# 左邊四個垂直單元
-x1, y1 = mychain.basic_rot(0+65*3, 0, 90)
-x2, y2 = mychain.basic_rot(x1, y1, 90)
-x3, y3 = mychain.basic_rot(x2, y2, 90)
-x4, y4 = mychain.basic_rot(x3, y3, 90)
-# 上方一個水平單元
-x5, y5 = mychain.basic_rot(x4, y4, 0)
-# 右斜 -40 度
-x6, y6 = mychain.basic_rot(x5, y5, -40)
-x7, y7 = mychain.basic_rot(x6, y6, -60)
-# 右中垂直向下單元
-x8, y8 = mychain.basic_rot(x7, y7, -90)
-# -120 度
-x9, y9 = mychain.basic_rot(x8, y8, -120)
-# -140
-x10, y10 = mychain.basic_rot(x9, y9, -140)
-# 水平接回原點
-mychain.basic(x10, y10, 0+65*3, 0, color="green")
-
-cgo.setWorldCoords(-120, -150, 500, 500) 
-# 畫 C
-# 上半部
-# 左邊中間垂直起點, 圓心位於線段中央, y 方向再向上平移兩個鏈條圓心距單位
-x1, y1 = mychain.basic_rot(0+65*2, -10+10+20*math.sin(80*deg)+20*math.sin(30*deg), 90)
-# 上方轉 80 度
-x2, y2 = mychain.basic_rot(x1, y1, 80)
-# 上方轉 30 度
-x3, y3 = mychain.basic_rot(x2, y2, 30)
-# 上方水平
-x4, y4 = mychain.basic_rot(x3, y3, 0)
-# 下半部, 從起點開始 -80 度
-x5, y5 = mychain.basic_rot(0+65*2, -10+10+20*math.sin(80*deg)+20*math.sin(30*deg), -80)
-# 下斜 -30 度
-x6, y6 = mychain.basic_rot(x5, y5, -30)
-# 下方水平單元
-x7, y7 = mychain.basic_rot(x6, y6, -0, color="green")
 </script>
-</body></html>
+</body>
+</html>
+
 '''
     return outstring
 
