5493423795f9f2a9e7b921a6ad7ecfdb060e30ed
40123133 <40123133@gm.nfu.edu.tw>
2016-06-28 23:21:48 +0800

    message

--- users/b/g1/b40123133_2D.py
+++ users/b/g1/b40123133_2D.py
@@ -1,23 +1,52 @@
 # 各組分別在各自的 .py 程式中建立應用程式 (第1步/總共3步)
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, make_response
+ 
 # 利用 Blueprint建立 scrum40123133_task1, 並且 url 前綴為 /bg1, 並設定 template 存放目錄
 scrum40123133_task1 = Blueprint('scrum40123133_task1', __name__, url_prefix='/bg1', template_folder='templates')
 
-# scrum40123133_task1 為完整可以單獨執行的繪圖程式
-@scrum40123133_task1.route('/scrum40123133_task1')
-def task1():
+@scrum40123133_task1.route('/circle')
+def three():
     outstring = '''
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>網際 2D 繪圖</title>
+    <!-- IE 9: display inline SVG -->
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+<script type="text/javascript" src="http://brython.info/src/brython_dist.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango-8v03.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango2D-6v13.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/CangoAxes-1v33.js"></script>
+ 
+</head>
+<body>
+ 
+<script>
+window.onload=function(){
+brython(1);
+}
+</script>
+ 
+<canvas id="plotarea" width="800" height="800"></canvas>
+
+<script type="text/python">
 from javascript import JSConstructor
+from browser import alert
 from browser import window
 import math
+ 
 cango = JSConstructor(window.Cango)
 cobj = JSConstructor(window.Cobj)
 shapedefs = window.shapeDefs
 obj2d = JSConstructor(window.Obj2D)
 cgo = cango("plotarea")
+ 
 cgo.setWorldCoords(-250, -250, 500, 500) 
-# 決定要不要畫座標軸線
+ 
+# 畫軸線
 cgo.drawAxes(0, 240, 0, 240, {
     "strokeColor":"#aaaaaa",
     "fillColor": "#aaaaaa",
@@ -25,55 +54,95 @@ cgo.drawAxes(0, 240, 0, 240, {
     "xLabelInterval": 20,
     "yTickInterval": 20,
     "yLabelInterval": 20})
-        
-#cgo.drawText("使用 Cango 繪圖程式庫!", 0, 0, {"fontSize":60, "fontWeight": 1200, "lorg":5 })
+ 
 deg = math.pi/180  
-def O(x, y, rx, ry, rot, color, border, linewidth):
-    # 旋轉必須要針對相對中心 rot not working yet
-    chamber = "M -6.8397, -1.4894 \
-                     A 7, 7, 0, 1, 0, 6.8397, -1.4894 \
-                     A 40, 40, 0, 0, 1, 6.8397, -18.511 \
-                     A 7, 7, 0, 1, 0, -6.8397, -18.511 \
-                     A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
+ 
+# 將繪製鏈條輪廓的內容寫成 class 物件
+class chain():
+    # 輪廓的外型設為 class variable
+    chamber = "M5.000 42.998 L10.399,42.998 L10.399,26.279 L30.397,26.279 L30.397,5.000 L5.397,5.000 L5.000,42.998 M12.442 9.993 L24.904,9.993 L24.904,20.789 L12.442,20.789 L12.442,9.993 "
+    #chamber = "M 0, 0 L 0, -20 z"
     cgoChamber = window.svgToCgoSVG(chamber)
-    cmbr = cobj(cgoChamber, "SHAPE", {
-            "fillColor": color,
-            "border": border,
-            "strokeColor": "tan",
-            "lineWidth": linewidth })
-    # 複製 cmbr, 然後命名為 basic1
-    basic40123133_A_1 = cmbr.dup()
-    basic40123133_A_1.rotate(90)
-    basic40123133_A_1.translate(-60, -20)
-    basic40123133_A_2 = cmbr.dup()
-    basic40123133_A_2.rotate(157.93)
-    basic40123133_A_2.translate(-77.51, 19.84)   
-    basic40123133_B_1 = cmbr.dup()
-    basic40123133_B_1.rotate(0)
-    basic40123133_B_1.translate(37.32, 30)
-    basic40123133_C_1 = cmbr.dup()
-    basic40123133_C_1.rotate(90)
-    basic40123133_C_1.translate(99.5, 40)
-    basic40123133_D_1 = cmbr.dup()
-    basic40123133_D_1.rotate(180)
-    basic40123133_D_1.translate(204.48, -10)
-    basic40123133_D_2 = cmbr.dup()
-    basic40123133_D_2.rotate(180)
-    basic40123133_D_2.translate(160, 20)
-    cmbr.appendPath(basic40123133_A_1)
-    cmbr.appendPath(basic40123133_A_2)
-    cmbr.appendPath(basic40123133_B_1)
-    cmbr.appendPath(basic40123133_C_1)
-    cmbr.appendPath(basic40123133_D_1)
-    cmbr.appendPath(basic40123133_D_2)
-    
-    # hole 為原點位置
-    hole = cobj(shapedefs.circle(4), "PATH")
-    cmbr.appendPath(hole)
-    # 表示放大 3 倍
-    #cgo.render(cmbr, x, y, 3, rot)
-    # 放大 5 倍
-    cgo.render(cmbr, x, y, 1, rot)
-O(0, 0, 0, 0, 0, "green", True, 4)
+ 
+    def __init__(self, fillcolor="green", border=True, strokecolor= "tan", linewidth=2, scale=1):
+        self.fillcolor = fillcolor
+        self.border = border
+        self.strokecolor = strokecolor
+        self.linewidth = linewidth
+        self.scale = scale
+ 
+    # 利用鏈條起點與終點定義繪圖
+    def basic(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        # 注意, cgo.Chamber 為成員變數
+        cmbr = cobj(self.cgoChamber, "SHAPE", {
+                "fillColor": self.fillcolor,
+                "border": self.border,
+                "strokeColor": self.strokecolor,
+                "lineWidth": self.linewidth })
+ 
+        # hole 為原點位置
+        hole = cobj(shapedefs.circle(4*self.scale), "PATH")
+        cmbr.appendPath(hole)
+ 
+        # 複製 cmbr, 然後命名為 basic1
+        basic1 = cmbr.dup()
+        # 因為鏈條的角度由原點向下垂直, 所以必須轉 90 度, 再考量 atan2 的轉角
+        basic1.rotate(math.atan2(y2-y1, x2-x1)/deg+90)
+ 
+        # 放大 scale 倍
+        cgo.render(basic1, x1, y1, self.scale, 0)
+ 
+    # 利用鏈條起點與旋轉角度定義繪圖, 使用內定的 color, border 與 linewidth 變數
+    def basic_rot(self, x1, y1, rot, v=False):
+        # 若 v 為 True 則為虛擬 chain, 不 render
+        self.x1 = x1
+        self.y1 = y1
+        self.rot = rot
+        self.v = v
+        # 注意, cgoChamber 為成員變數
+        cmbr = cobj(self.cgoChamber, "SHAPE", {
+                "fillColor": self.fillcolor,
+                "border": self.border,
+                "strokeColor": self.strokecolor,
+                "lineWidth": self.linewidth })
+ 
+        # hole0 為原點位置
+        hole = cobj(shapedefs.circle(4*self.scale), "PATH")
+        cmbr.appendPath(hole)
+        # 根據旋轉角度, 計算 x2 與 y2
+        x2 = x1 + 20*math.cos(rot*deg)*self.scale
+        y2 = y1 + 20*math.sin(rot*deg)*self.scale
+ 
+        # 複製 cmbr, 然後命名為 basic1
+        basic1 = cmbr.dup()
+        # 因為鏈條的角度由原點向下垂直, 所以必須轉 90 度, 再考量 atan2 的轉角
+        basic1.rotate(rot+90)
+ 
+        # 放大 scale 倍
+        if v == False:
+            cgo.render(basic1, x1, y1, self.scale, 0)
+ 
+        return x2, y2
+
+mychain = chain()
+ 
+x1, y1 = mychain.basic_rot(0,0, 50)
+x2, y2=mychain.basic_rot(x1, y1, 90-80) 
+x3, y3=mychain.basic_rot(x2, y2, 90-120) 
+x4, y4=mychain.basic_rot(x3, y3, 90-160) 
+x5, y5=mychain.basic_rot(x4, y4, 90-200) 
+x6, y6=mychain.basic_rot(x5, y5, 90-240) 
+x7, y7=mychain.basic_rot(x6, y6, 90-280) 
+x8, y8=mychain.basic_rot(x7, y7, 90-320) 
+x9, y9=mychain.basic_rot(x8, y8, 90-360) 
+
+</script>
+</body>
+</html>
+
 '''
     return outstring
